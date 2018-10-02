c0f8ee77d2a8a3cf92063a9898de6adce2b31c11
rednes <528249+rednes@users.noreply.github.com>
2018-10-02 14:19:46 +0900

    deployスクリプトのフォルダリストが出力順に依存していたため、.と..を明示的に除外するよう修正

--- deploy/deploy.rb
+++ deploy/deploy.rb
@@ -20,48 +20,50 @@ if Dir.exist?(ITEMS_DIR) then
   Dir.chdir(ITEMS_DIR)
   # 記事保存ディレクトリのフルパス記録
   ITEMS_DIR_FULL_PATH = Dir.pwd
-  p ITEMS_DIR_FULL_PATH
-  p Dir::entries(ITEMS_DIR_FULL_PATH)
 
   # '.'と'..'を除いて全フォルダに対してDeploy実行
-  for dir in Dir::entries(ITEMS_DIR_FULL_PATH)[2..-1] do
-    p '[' + dir + ']'
-    # 各記事フォルダに移動
-    Dir.chdir(dir)
-
-    # LOCKが存在したらデプロイしない
-    if File.exist?(LOCK_FILE_NAME) then
-      p 'LOCK is existed. No deploy.'
+  Dir.foreach(ITEMS_DIR_FULL_PATH){|dir|
+    if dir=='.' or dir=='..' then
+      p dir + ' is skipped.'
     else
-      # 記事用のパラメータファイル読み込み
-      params = File.open(PARAMS_FILE_NAME) do |file|
-        JSON.load(file)
-      end
+      p '[' + dir + ']'
+      # 各記事フォルダに移動
+      Dir.chdir(dir)
 
-      # Headersを設定
-      headers = {'Content-Type' => 'application/json'}
+      # LOCKが存在したらデプロイしない
+      if File.exist?(LOCK_FILE_NAME) then
+        p 'LOCK is existed. No deploy.'
+      else
+        # 記事用のパラメータファイル読み込み
+        params = File.open(PARAMS_FILE_NAME) do |file|
+          JSON.load(file)
+        end
 
-      # 記事ファイル読み込み
-      body = File.open(BODY_FILE_NAME) do |file|
-        file.read
-      end
+        # Headersを設定
+        headers = {'Content-Type' => 'application/json'}
 
-      # 記事ファイルを設定
-      params['body'] = body
-
-      # ITEM_IDが存在したらその記事IDで記事を更新する。
-      # 存在しなかったら新しい記事を作成する。
-      if File.exist?(ITEM_ID_FILE_NAME) then
-        item_id = File.open(ITEM_ID_FILE_NAME) do |file|
+        # 記事ファイル読み込み
+        body = File.open(BODY_FILE_NAME) do |file|
           file.read
         end
-        p client.update_item(item_id, params, headers)
-      else
-        p client.create_item(params, headers)
-      end
-    end
 
-    # 記事保存ディレクトリに戻る
-    Dir.chdir(ITEMS_DIR_FULL_PATH)
-  end
+        # 記事ファイルを設定
+        params['body'] = body
+
+        # ITEM_IDが存在したらその記事IDで記事を更新する。
+        # 存在しなかったら新しい記事を作成する。
+        if File.exist?(ITEM_ID_FILE_NAME) then
+          item_id = File.open(ITEM_ID_FILE_NAME) do |file|
+            file.read
+          end
+          p client.update_item(item_id, params, headers)
+        else
+          p client.create_item(params, headers)
+        end
+      end
+
+      # 記事保存ディレクトリに戻る
+      Dir.chdir(ITEMS_DIR_FULL_PATH)
+    end
+  }
 end
